221111d8c362f6e298e1ec3d6a1e7d2f5839857b
Gert Vanbeckevoort <gert.vanbeckevoort@gmail.com>
2018-02-26 13:33:14 +0100

    taking care of backup locations

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -1,5 +1,15 @@
 ;;HERE MY PRIVATE SETTINGS
 
+;;auto-save and backup
+(setq backup-by-copying t      ; don't clobber symlinks
+      backup-directory-alist '(("." . "~/.emacs.d/backup/"))  ; don't litter my fs tree
+      delete-old-versions t
+      kept-new-versions 6
+      kept-old-versions 2
+      version-control t)       ; use versioned backups
+(setq auto-save-file-name-transforms
+      `((".*" "~/.emacs.d/backup/" t)))
+
 (require 'init-lilypond)
 (require 'init-xmlformat)
 
