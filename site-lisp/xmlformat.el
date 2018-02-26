a8e77df0c0ff01410620e40d067439f4e99eeae6
Gert Vanbeckevoort <gert.vanbeckevoort@gmail.com>
2018-02-26 11:19:39 +0100

    forgot the el-file

---
+++ site-lisp/xmlformat.el
@@ -0,0 +1,13 @@
+(defun format-xml ()
+  "Excecutes shell with current buffer as input"
+  (interactive)
+  (shell-command-on-region
+   (point-min)
+   (point-max)
+   "xmllint --format -"
+   (current-buffer)
+   t
+   "*XMLFormat Error Buffer*"
+   t))
+
+(provide 'xmlformat)
